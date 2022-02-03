bac22acd837706bc94d73771732c5ed46e6207eb
Daneric <danericaalbers@hotmail.com>
2022-02-03 12:26:47 +0100

    feat: tv routes

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express = require('express');
 const app = express();
 const config = require('./config');
 const path = require('path');
+const axios = require('axios')
+const port = config.port || 3000;
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, '/views'));
@@ -13,6 +15,19 @@ app.get('/', async (req, res) => {
         config: config
     });
 });
+
+app.get('/tv/:ip', async (req, res) => {
+    res.render('tv', {
+        title: 'TV',
+        ip: req.params.ip,
+    });
+})
+app.get('/tv/:ip/post', async (req, res) => {
+    var ip = req.params.ip;
+    var command = req.query.command;
+    axios.post(`${config.api}/tv/${ip}`, { "command": command })
+    res.redirect(`/tv/${ip}`)
+})
 
 // Launch app on port
 app.listen(port, () => console.log('\x1b[31m%s\x1b[0m', '[SERVER]', '\x1b[32m[WEB]\x1b[0m', `Connected @ localhost:${port}`));
